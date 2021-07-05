e46aeded163765f259053ee326d0b135e0e4dff7
sandeep <bssandeep150@gmail.com>
2021-07-05 18:39:40 +0530

    git earch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'search';
+  keyword="";
+  keyworddata="";
+  items:any;
+  show:any;
+  totalLength:any;
+  page:number=1;
+  showpost:any=[];
+
+  constructor(private http:HttpClient){}
+ 
+
+  onClick(value:any){
+    this.keyword=value;
+    console.log(this.keyword);
+    this.getRepositoryData().subscribe(data=>{
+      console.log("sandeep");
+      console.log(data.items);
+      this.showpost=data.items;
+      this.items=data.items;
+      this.totalLength=data.items.length;;
+      })
+    }
+    
+
+
+
+
+
+
+
+  getRepositoryData(){
+    let url=`https://api.github.com/search/repositories?q=${this.keyword}`;
+     return this.http.get<any>(url);
+  }
 }
